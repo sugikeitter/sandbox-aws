82ee8b00bd2161dd20ddd71ba65069e60c79cf77
sugikeitter <s.l.n.f.13_kei.s@me.com>
2021-01-01 14:15:11 +0900

    Update create_python_and_fastapi_env.sh

--- shell/userdata/create_python_and_fastapi_env.sh
+++ shell/userdata/create_python_and_fastapi_env.sh
@@ -13,7 +13,19 @@ pip install fastapi uvicorn boto3
 # FastAPIの最小構成作成のコードを作成して起動
 cd /home/ec2-user/my_app/
 
-https://raw.githubusercontent.com/sugikeitter/sandbox-aws/main/python/fastapi_restapp.py
+wget https://raw.githubusercontent.com/sugikeitter/sandbox-aws/main/python/fastapi_restapp.py
+
+cat <<EOL >> /etc/logrotate.d/app
+/home/ec2-user/my_app/rest_app.log
+{
+  missingok
+  rotate 7
+  dateext
+  dateformat _%Y%m%d
+  0644 root root
+  daily
+}
+EOL
 
 # fastapiの起動
-nohup uvicorn fastapi_restapp:app --host 0.0.0.0 > rest_app.log &
+nohup uvicorn fastapi_restapp:app --host 0.0.0.0 > /home/ec2-user/my_app/rest_app.log &
